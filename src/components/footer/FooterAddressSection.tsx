3a1f356b16e96707faa13fc63b505832de202711
mofesilva <mofesilva.dev@gmail.com>
2025-07-24 10:10:22 -0300

    Atualizada a seção FooterAddressSection para centralizar o conteúdo e ajustar o espaçamento, melhorando a apresentação visual da seção de endereços.

--- src/components/footer/FooterAddressSection.tsx
+++ src/components/footer/FooterAddressSection.tsx
@@ -4,7 +4,7 @@ import AddressItem from './AddressItem';
 function FooterAddressSection() {
     return (
         <>
-            <section className="flex gap-8 items-start justify-start h-auto p-10 pl-0">
+            <section className="flex gap-8 items-start justify-center self-center h-auto px-4">
                 <AddressItem title="CAMPINAS" address="Av. Ântonio Artioli, 570" details="Lojas 07 e 08 - Swiss Park" city="Campinas" state='SP' country="Brasil" />
                 <AddressItem title="BELO HORIZONTE" address="Rua Professor Pimenta da Veiga, 78" details="Cidade Nova" city="Belo Horizonte" state="MG" country="Brasil" />
                 <AddressItem title="LEGNANO" address="Av. Joaquim José Diniz, 20, 905" details="Cidade Nova" city="Legnano" state="Milano" country="Italia" />
