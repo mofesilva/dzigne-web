4ea0bad82b52b914fcf6eb33efde21fa7c0f2da7
mofesilva <mofesilva.dev@gmail.com>
2025-07-24 10:10:29 -0300

    Adicionado o componente FooterNavigationSection, que exibe links de navegação no rodapé, melhorando a estrutura e a usabilidade da seção de rodapé.

---
+++ src/components/footer/FooterNavigationSection.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Link from 'next/link';
+
+function FooterNavigationSection() {
+    return (
+        <>
+            <div className="font-rajdhani text-lg font-semibold text-carbon self-center">
+                <ul className="">
+                    <li className="">
+                        <Link href="/" className="footer__menu__link">
+                            Contato
+                        </Link>
+                    </li>
+                    <li className="">
+                        <Link href="/about-us" className="">
+                            Inside Dzign-e
+                        </Link>
+                    </li>
+                    <li className="">
+                        <a href="/cases" className="">
+                            Cases
+                        </a>
+                    </li>
+                    <li className="">
+                        <a href="/plataformas" className="">
+                            Plataformas
+                        </a>
+                    </li>
+                    <li className="">
+                        <a href="/politicas-de-privacidade" className="">
+                            Políticas de privacidade
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default FooterNavigationSection;
