97d6b4096f44d5fbcabdd218cdf6948eb5b91a49
mofesilva <mofesilva.dev@gmail.com>
2025-07-24 10:10:09 -0300

    Adicionado o componente Carousel, que implementa um carrossel de slides utilizando a biblioteca Swiper, com suporte a navegação, paginação e autoplay.

---
+++ src/components/Swiper.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
+import type { SwiperOptions } from 'swiper/types'
+import 'swiper/css/bundle' // CSS completo do Swiper
+
+type CarouselProps = {
+    slides: React.ReactNode[]
+    options?: SwiperOptions
+    className?: string
+}
+
+export default function Carousel({ slides, options, className }: CarouselProps) {
+    return (
+        <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            navigation
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 4000 }}
+            loop
+            {...options}
+            className={className}
+        >
+            {slides.map((node, i) => (
+                <SwiperSlide key={i}>{node}</SwiperSlide>
+            ))}
+        </Swiper>
+    )
+}
