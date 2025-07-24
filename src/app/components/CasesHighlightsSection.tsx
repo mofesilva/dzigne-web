2a2d5762a7b103bc67feee7fa2a3acd49a337a35
mofesilva <mofesilva.dev@gmail.com>
2025-07-24 10:08:34 -0300

    Adicionado o componente CasesHighlightsSection com um Carousel vazio para exibição de destaques.

---
+++ src/app/components/CasesHighlightsSection.tsx
@@ -0,0 +1,12 @@
+import Carousel from '@/components/Swiper';
+import React from 'react'
+
+function CasesHighlightsSection() {
+    return (
+        <section>
+            <Carousel slides={[]} />
+        </section>
+    )
+}
+
+export default CasesHighlightsSection;
