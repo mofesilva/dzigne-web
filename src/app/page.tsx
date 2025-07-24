e2ef26b6a2b7c4e5ee620ae9b963f35e9080a068
mofesilva <mofesilva.dev@gmail.com>
2025-07-24 10:08:19 -0300

    Adicionada a seção CompanyHighlights ao componente Home, melhorando a apresentação das informações da empresa.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeaderSection from "./components/HeaderSection";
 import Footer from "@/components/footer/Footer";
+import CompanyHighlightsSection from "./components/CompanyHighlightsSection";
 export default function Home() {
   return (
     <main className="flex flex-col">
       <div className="">
         <HeaderSection />
+        <CompanyHighlightsSection />
         <Footer />
       </div>
     </main>
