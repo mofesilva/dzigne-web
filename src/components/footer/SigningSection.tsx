5caf90ee8c78a22dbce024f719dafce6c35eb905
mofesilva <mofesilva.dev@gmail.com>
2025-07-24 10:10:56 -0300

    Adicionado o componente SigningSection, que exibe uma mensagem de agradecimento e informações da empresa no rodapé, contribuindo para a identidade visual e a apresentação da seção.

---
+++ src/components/footer/SigningSection.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react';
+
+function SigningSection() {
+    return (
+        <div className="signing self-center">
+            <p className='font-rajdhani text-center text-carbon font-semibold'>Made with <i className="fa-brands fa-beat fa-react"></i> by Dzign-e Sistemas e Tecnolgia LTDA
+                16.950.862/0001-15 - Todas as imagens pertencem a Freepik
+            </p>
+        </div>
+    )
+}
+
+export default SigningSection;
