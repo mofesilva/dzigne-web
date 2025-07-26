d3150c1b92850520942f1af081b2e2f65f5252ad
mofesilva <mofesilva.dev@gmail.com>
2025-07-25 21:23:59 -0300

    Atualizada a seção FooterContactSection para melhorar a responsividade e a apresentação visual, centralizando o conteúdo em dispositivos menores e ajustando o espaçamento entre os elementos.

--- src/components/footer/FooterContactSection.tsx
+++ src/components/footer/FooterContactSection.tsx
@@ -5,13 +5,13 @@ import Image from 'next/image';
 function FooterContactSection() {
     return (
         <>
-            <section className="flex flex-col items-start justify-start h-auto p-10 pl-0">
+            <section className="flex flex-col items-center md:items-start justify-start h-auto pt-10 pb-0 px-10 md:p-10 md:pl-0 text-center md:text-left">
                 <Image src='/assets/dzigne-logotype-black.png'
                     alt="Logotipo Dzign-e"
                     width={100}
                     height={100}
                     className="h-auto w-30 pb-4" />
-                <div className="pl-0 items-start text-start justify-start pb-4">
+                <div className="flex items-center justify-center md:justify-start pb-4 gap-2">
                     <a href="https://www.instagram.com/dzign.e/"
                         target="_blank"
                         rel="noopener noreferrer">
@@ -23,14 +23,14 @@ function FooterContactSection() {
                         <i className="fa-brands fa-linkedin-in text-carbon text-2xl"></i>
                     </a>
                 </div>
-                <div className="pb-4">
+                <div className="pb-4 flex flex-col items-center md:items-start gap-2">
                     <div className="flex items-center gap-1">
                         <i className="fa-solid fa-at text-carbon text-xl"></i>
                         <a href="mailto:contato@dzign-e.com.br" className="font-league-spartan text-xl text-carbon">
                             contato@dzign-e.com.br
                         </a>
                     </div>
-                    <div className="flex items-center gap-1 ">
+                    <div className="flex items-center gap-1">
                         <i className="fa-brands fa-whatsapp text-carbon text-xl font-bold"></i>
                         <a
                             href="https://wa.me/5519999094848"
