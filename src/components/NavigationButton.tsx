354142fa668e1e1af45c5e16ada3bff654068534
mofesilva <mofesilva.dev@gmail.com>
2025-07-26 19:31:46 -0300

    Adicionada a propriedade 'inverted' ao componente NavigationButton, permitindo personalização adicional do estilo do botão de navegação com base na interação do usuário.

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -6,12 +6,13 @@ interface NavigationButtonProps {
     href: string
     text: string
     icon?: string
+    inverted?: boolean
 }
 
-function NavigationButton({ href, text, icon }: NavigationButtonProps) {
+function NavigationButton({ href, text, icon, inverted = false }: NavigationButtonProps) {
     return (
-        <Link href={href} className={`group bg-green-accent ${icon ? 'pr-4 pl-6' : 'px-6'} py-2 rounded-lg border-2 border-transparent hover:bg-transparent hover:border-green-accent transition-colors duration-300 inline-block cursor-pointer`}>
-            <span className="text-carbon group-hover:text-green-accent font-rajdhani font-semibold">
+        <Link href={href} className={`group ${inverted ? 'bg-carbon hover:border-carbon' : 'bg-green-accent hover:border-green-accent'} ${icon ? 'pr-4 pl-6' : 'px-6'} py-2 rounded-lg border-2 border-transparent hover:bg-transparent transition-colors duration-300 inline-block cursor-pointer`}>
+            <span className={`${inverted ? 'text-eggshell group-hover:text-carbon' : 'text-carbon group-hover:text-green-accent'} font-rajdhani font-semibold`}>
                 {text} {icon && <i className={`fa-solid ${icon}`}></i>}
             </span>
         </Link>
