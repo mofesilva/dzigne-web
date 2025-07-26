6af509ef1eeb0358e91c6753afc5f24f4c241c88
mofesilva <mofesilva.dev@gmail.com>
2025-07-25 21:25:57 -0300

    Atualizado o componente CompanyBio para incluir um botão de navegação e melhorar a estrutura visual, ajustando margens e classes de estilo para uma apresentação mais atraente.

--- src/app/components/CompanyBio.tsx
+++ src/app/components/CompanyBio.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import NavigationButton from '../../components/NavigationButton';
 
 function CompanyBio() {
     return (
-        <section aria-label="Conheça a Dzign-e" className='section-container py-10'>
-            <div className="about__us__container">
-                <div className="about__us__content">
-                    <h1 className="about__us__title">Somos a Dzign-e</h1>
-                    <p className="about__us__paragraph">Uma software house brasileira especializada em criar produtos e soluções
+        <section aria-label="Conheça a Dzign-e" className='section-container py-20'>
+            {/* Header com título e botão */}
+            <div className="flex justify-between items-start mb-16">
+                <div>
+                    <h1 className="font-rajdhani text-5xl font-bold text-white mb-4">
+                        Somos a Dzign-e
+                    </h1>
+                    <p className="font-league-spartan text-xl pb-8 text-white/70 max-w-2xl">
+                        Uma software house brasileira especializada em criar produtos e soluções
                         digitais
                         sob medida para empresas que buscam inovação,
                         eficiência e resultados reais. Combinamos design inteligente, tecnologia de ponta e uma escuta ativa
@@ -14,13 +19,15 @@ function CompanyBio() {
                         cliente para transformar ideias em sistemas, apps e plataformas robustas.
                         Nosso compromisso vai além do código: entregamos projetos com propósito, que evoluem negócios e
                         simplificam a vida das
-                        pessoas.</p>
-                    <h2 className="about__us__subtitle">“Na Dzign-e, unimos inteligência técnica e sensibilidade de negócio para
-                        entregar soluções digitais que crescem com
-                        você.”</h2>
-                    <a href="/about-us.html" className="about__button button">
-                        Inside Dzign-e
-                    </a>
+                        pessoas.
+                    </p>
+
+                    <div className="flex flex-col sm:flex-row gap-4 justify-center text-center mb-8">
+                        <NavigationButton
+                            href={'/about-us'}
+                            text={'Conheça a Dzign-e'}
+                        />
+                    </div>
                 </div>
             </div>
         </section>
