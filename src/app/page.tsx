9631282749509b1b7f59f8afe91932d87385d7ac
Moisés Silva <mofesilva.dev@gmail.com>
2025-08-02 17:02:01 -0300

    Remover importação do componente Footer da página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import HeaderSection from "./components/HeaderSection";
-import Footer from "@/components/footer/Footer";
 import CompanyHighlightsSection from "./components/CompanyHighlightsSection";
 import HeroSection from "./components/HeroSection";
 import CasesHighlightsSection from "./components/CasesHighlightsSection";
