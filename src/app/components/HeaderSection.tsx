f06f77c778566102e0e1ec2f93ee6084e51aec7f
mofesilva <mofesilva.dev@gmail.com>
2025-08-06 10:26:22 -0300

    Atualizar estilo da seção HeaderSection, ajustando a classe de fundo e o preenchimento superior para melhorar a apresentação visual e responsividade.

--- src/app/components/HeaderSection.tsx
+++ src/app/components/HeaderSection.tsx
@@ -8,10 +8,7 @@ interface HeaderSectionProps {
 
 function HeaderSection({ content, isSecondaryPage = false }: HeaderSectionProps) {
     return (
-        <section className={`w-full ${isSecondaryPage ? 'min-h-[50vh]' : 'min-h-screen'} relative overflow-hidden`}>
-            {/* Fundo preto */}
-            <div className="absolute inset-0 bg-carbon"></div>
-
+        <section className={`pt-16 bg-carbon lg:rounded-4xl m-0 lg:m-2 lg:mb-0 ${isSecondaryPage ? 'min-h-[50vh]' : 'min-h-screen'}`}>
             <div className='relative z-10 w-full h-full'>
                 <div className="section-container h-full flex flex-col justify-center">
                     <div className={`${isSecondaryPage ? 'mt-8' : 'mt-20'}`}>
