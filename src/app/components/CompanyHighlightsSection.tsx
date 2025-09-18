30deeea41709f808e62e38e5b9d62e20c66561fe
mofesilva <mofesilva.dev@gmail.com>
2025-09-18 18:24:41 -0300

    Ajustar estilos e layout do componente CompanyHighlightsSection

--- src/app/components/CompanyHighlightsSection.tsx
+++ src/app/components/CompanyHighlightsSection.tsx
@@ -3,15 +3,15 @@ import Image from 'next/image';
 
 function CompanyHighlightsSection() {
     return (
-        <section className="bg-eggshell w-full h-auto md:h-[200px] flex items-center py-8 md:py-0">
-            <div className="section-container flex flex-col md:flex-row items-center md:justify-between justify-center w-full min-w-0 gap-6 md:gap-0">
+        <section className="bg-eggshell w-auto h-auto flex items-center py-8 lg:py-16">
+            <div className="section-container flex flex-col lg:flex-row items-center md:justify-between justify-center w-full min-w-0 gap-6 md:gap-0">
                 <h1 className="text-3xl font-rajdhani font-bold text-carbon flex-shrink-0 text-center md:text-left">Empresas que inovaram <br />com a nossa
                     <span className="bg-green-accent text-carbon ml-1 px-2 py-1">expertise!</span>
                 </h1>
-                <div className="flex gap-6 md:gap-16 items-center flex-shrink">
-                    <Image src='/assets/images/logos/labormed.png' alt="Labormed Logo" width={120} height={120} className="w-20 md:w-[120px] h-auto" />
-                    <Image src='/assets/images/logos/arion.png' alt="Arion Logo" width={120} height={120} className="w-20 md:w-[120px] h-auto" />
-                    <Image src='/assets/images/logos/mackenzie.png' alt="Mackenzie Logo" width={120} height={120} className="w-20 md:w-[120px] h-auto" />
+                <div className="flex pt-2 lg:pt-0 justify-between md:gap-16 items-center flex-shrink w-full lg:w-auto">
+                    <Image src='/assets/images/logos/labormed.png' alt="Labormed Logo" width={120} height={120} className="h-auto" />
+                    <Image src='/assets/images/logos/arion.png' alt="Arion Logo" width={120} height={120} className="h-auto" />
+                    <Image src='/assets/images/logos/mackenzie.png' alt="Mackenzie Logo" width={120} height={120} className="h-auto" />
                 </div>
             </div>
         </section>
