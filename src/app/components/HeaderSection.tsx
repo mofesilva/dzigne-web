25918f071fb52dc33350bab5d91f1b4575c26abd
mofesilva <mofesilva.dev@gmail.com>
2025-09-18 18:24:44 -0300

    Ajustar estilos do componente HeaderSection para melhor responsividade

--- src/app/components/HeaderSection.tsx
+++ src/app/components/HeaderSection.tsx
@@ -8,7 +8,7 @@ interface HeaderSectionProps {
 
 function HeaderSection({ content, isSecondaryPage = false }: HeaderSectionProps) {
     return (
-        <section className={`pt-16 bg-carbon lg:rounded-4xl m-0 lg:m-2 lg:mb-0 ${isSecondaryPage ? 'min-h-[50vh]' : 'min-h-screen'}`}>
+        <section className={`py-16 bg-carbon sm:rounded-4xl m-0 sm:m-2 lg:mb-0 h-auto`}>
             <div className='relative z-10 w-full h-full'>
                 <div className="section-container h-full flex flex-col justify-center">
                     <div className={`${isSecondaryPage ? 'mt-8' : 'mt-20'}`}>
