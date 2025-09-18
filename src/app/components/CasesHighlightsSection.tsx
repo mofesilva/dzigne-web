b493617e4bec24767546d13d6da9ea1680bf7ef6
mofesilva <mofesilva.dev@gmail.com>
2025-09-18 18:24:35 -0300

    Atualizar a seção CasesHighlights com novos casos e logos, além de ajustes de estilo

--- src/app/components/CasesHighlightsSection.tsx
+++ src/app/components/CasesHighlightsSection.tsx
@@ -4,10 +4,10 @@ import CaseSlide from './CaseSlide';
 
 function CasesHighlightsSection() {
     return (
-        <section className=" bg-carbon lg:mx-2" style={{ minHeight: 600 }}>
+        <section className=" bg-carbon sm:mx-2">
             <div className="section-container flex justify-center items-center">
                 <Carousel
-                    className="h-auto md:h-[600px] w-full"
+                    className="h-auto w-full"
                     style={{
                         '--swiper-navigation-color': '#7fd10c',
                         '--swiper-pagination-bullet-inactive-color': '#eeeeee',
@@ -24,21 +24,38 @@ function CasesHighlightsSection() {
                             dos alunos para a OAB, integrando simulados intuitivos
                             com gestão dinâmica do conteúdo pelos professores.
                             Descubra como revolucionamos a preparação dos estudantes."
-                            logo="/assets/images/logos/mackenzie1.png"
-                            alt="Labormed Logo"
+                            logo="/assets/images/logos/mackenzie2.png"
+                            alt="Mackenzie Logo"
+                            isLogoHorizontal={true}
                             mockup="/assets/images/mockups/oabmack_mockup.png"
-                            href="/" />,
-                        <CaseSlide key={2} text="Desenvolvemos um app que revolucionou a gestão condominial,
+                            href="/cases/oab-mack" />,
+                        <CaseSlide key={2} text="Criamos o sistema oficial da Igreja Presbiteriana do Brasil, o iCalvinus, um sistema de gestão que integra diversas funcionalidades cadastrais, documentos e gestão conciliar. Com essa solução, a IPB conseguiu otimizar o acesso aos documentos e decisões e ainda reduziu o tempo das reuniões desta celere denominação evangelica."
+                            logo="/assets/images/logos/icalvinus-hn.png"
+                            alt="Icalvinus Logo"
+                            isLogoHorizontal={true}
+                            mockup="/assets/images/mockups/iCalvinus_mockup_2.png"
+                            href="/cases/icalvinus" />,
+                        <CaseSlide key={3} text="Desenvolvemos um aplicativo que revolucionou a gestão condominial,
                             integrando reservas, câmeras e autorizações de entrada em
-                            uma única solução inteligente.
+                            uma única solução inteligente, facilitando a vida dos moradores de diversos condomínios pelo Brasil."
+                            logo="/assets/images/logos/sigmaapp-n.png"
+                            alt="Sigmacon Logo"
+                            isLogoHorizontal={true}
+                            mockup="/assets/images/mockups/sigmaapp-mockup-1.png"
+                            href="/cases/sigmaapp" />,
+                        <CaseSlide key={4} text="Contrata já é um marketplace em desenvolvimento, para conectar prestadores de serviços a potenciais clientes. Com uma interface intuitiva e funcionalidades robustas, a plataforma visa facilitar o processo de contratação, oferecendo segurança e eficiência tanto para profissionais quanto para usuários finais."
+                            logo="/assets/images/logos/contrataja-hn1.png"
+                            alt="Contrata Já Logo"
+                            isLogoHorizontal={true}
+                            mockup="/assets/images/mockups/contrataja-ipad-mockup.png"
+                            href="/cases/contrata-ja" />,
+                        <CaseSlide key={5} text="Criamos o sistema oficial da Igreja Presbiteriana do Brasil, o iCalvinus, um sistema de gestão que integra diversas funcionalidades cadastrais, documentos e gestão conciliar. Com essa solução, a IPB conseguiu otimizar o acesso aos documentos e decisões e ainda reduziu o tempo das reuniões desta celere denominação evangelica."
+                            logo="/assets/images/logos/ipiconecta-h2.png"
+                            alt="Icalvinus Logo"
+                            isLogoHorizontal={true}
+                            mockup="/assets/images/mockups/ipiconecta_desktop_mockup_1.png"
+                            href="/cases/ipiconecta" />,
 
-                            Clique e veja como a tecnologia virou aliada da segurança." logo="/assets/images/logos/sigmacon1png.png" alt="Arion Logo" mockup="/assets/images/mockups/oabmack_mockup.png" href="/" />,
-                        <CaseSlide key={3} text="Auxiliamos a Universidade Mackenzie no desenvolvimento uma
-                            plataforma personalizada, composta por um aplicativo móvel para alunos e
-                            uma interface web para
-                            professores. A solução proporcionou uma experiência intuitiva e produtiva de estudos,
-                            permitindo gestão eficiente do
-                            conteúdo e resultando em uma preparação mais robusta para os estudantes." logo="/assets/images/logos/ipi1.png" alt="Mackenzie Logo" mockup="/assets/images/mockups/oabmack_mockup.png" href="/" />
                     ]}
                 />
             </div>
