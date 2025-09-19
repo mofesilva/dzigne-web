b46d0eb05c66d61f60b62227e7ec5f7229ce9abc
mofesilva <mofesilva.dev@gmail.com>
2025-09-19 12:21:28 -0300

    Atualizar página do caso ICalvinus com novas informações, ajustar tecnologias e desafios, e comentar seção de galeria

--- src/app/cases/icalvinus/page.tsx
+++ src/app/cases/icalvinus/page.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-// import Footer from '@/components/footer/Footer';
-// import SecondaryHeaderSection from '@/components/SecondaryHeaderSection';
 import CaseHeroSection from '../components/CaseHeroSection';
 import CaseDetailsSection from '../components/CaseDetailsSection';
 import CaseGallerySection from '../components/CaseGallerySection';
 import CaseNavigationSection from '../components/CaseNavigationSection';
 import HeaderSection from '@/app/components/HeaderSection';
-
-export const metadata = {
-    title: "Case Sigmacon - App de Gestão Condominial | Dzign-e",
-    description: "Como desenvolvemos um aplicativo revolucionário para gestão condominial integrando IoT, reservas e segurança"
-};
 
 function ICalvinusCasePage() {
     return (
@@ -18,7 +11,7 @@ function ICalvinusCasePage() {
             <HeaderSection
                 content={
                     <CaseHeroSection
-                        logo="/assets/images/logos/sigmaapp-n.png"
+                        logo="/assets/images/logos/icalvinus-hn.png"
                         company="Igreja Presbiteriana do Brasil"
                         description="Criamos o sistema oficial da Igreja Presbiteriana do Brasil."
                         tags={["Gestão", "Mobile App", "IoT", "Flutter", "Firebase"]}
@@ -29,12 +22,12 @@ function ICalvinusCasePage() {
             <CaseDetailsSection
                 duration="10 meses"
                 team="8 desenvolvedores"
-                technologies={["Flutter", "Firebase", "Node.js", "MongoDB", "AWS IoT", "WebRTC", "TypeScript"]}
-                challenge="Os condomínios enfrentavam dificuldades para gerenciar reservas de espaços comuns, controlar acessos, monitorar segurança e manter comunicação eficiente entre moradores e administração. Era necessária uma solução integrada que centralizasse todas essas funcionalidades em uma plataforma móvel intuitiva."
+                technologies={["Flutter", "Firebase", "Node.js", "MySQL", "Akamai Cloud"]}
+                challenge="A Igreja Presbiteriana do Brasil, tendo seu modelo de governo representativo, enfrentava uma enorme dificuldade de gerenciar as informações e documentos de decisões das reuniões conciliares. Todos os documentos e decisões eram impressos e armazenados na Secretaria Executiva. Essa instituições precisava de um sistema que centralizasse todos os documentos passados e futuro, de forma a providenciar o fácil acesso assim como  ."
                 solution="Desenvolvemos um aplicativo móvel completo que permite aos moradores reservar espaços comuns, visualizar câmeras de segurança, autorizar visitantes, receber comunicados da administração e acessar serviços do condomínio. Para administradores, criamos um painel de gestão com controle total das funcionalidades, relatórios e monitoramento em tempo real."
                 results={[
-                    "95% de redução no tempo de reserva de espaços",
-                    "Mais de 150 condomínios utilizando a plataforma",
+                    "Redução de 2/3 do tempo de reuniões conciliares",
+                    "Mais de 2800 documentos, decisões, ementas concentradas na plataforma",
                     "80% de aumento na satisfação dos moradores",
                     "Redução de 70% em conflitos por reservas",
                     "Sistema de segurança 24/7 implementado",
@@ -42,7 +35,7 @@ function ICalvinusCasePage() {
                 ]}
             />
 
-            <CaseGallerySection
+            {/* <CaseGallerySection
                 title="Funcionalidades Inovadoras"
                 description="Explore as principais funcionalidades que transformaram a gestão condominial e a experiência dos moradores."
                 images={[
@@ -57,18 +50,18 @@ function ICalvinusCasePage() {
                         description: "Interface intuitiva para reserva de espaços comuns com calendário interativo"
                     }
                 ]}
-            />
+            /> */}
 
             <CaseNavigationSection
                 previousCase={{
-                    href: "/cases/mackenzie",
-                    company: "Universidade Mackenzie",
-                    title: "Plataforma de Estudos para OAB"
+                    href: "/cases/oab-mack",
+                    company: "OAB Mack",
+                    title: "Universidade Presbiteriana Mackenzie"
                 }}
                 nextCase={{
-                    href: "/cases/ipi",
-                    company: "IPI - Igreja Presbiteriana Independente",
-                    title: "Plataforma Educacional Personalizada"
+                    href: "/cases/sigmaapp",
+                    company: "SigmaApp",
+                    title: "Sigmacon"
                 }}
             />
 
