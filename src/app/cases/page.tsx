c190dcd36ec59298732673ff43458041d8d78fa5
mofesilva <mofesilva.dev@gmail.com>
2025-09-19 12:21:16 -0300

    Remover importações não utilizadas do componente Page

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,6 +1,4 @@
-import Footer from "@/components/footer/Footer";
 import React from "react";
-import SecondaryHeaderSection from "../../components/SecondaryHeaderSection";
 import CasesHeroSection from "./components/CasesHeroSection";
 import CasesListSection from "./components/CasesListSection";
 import HeaderSection from "../components/HeaderSection";
