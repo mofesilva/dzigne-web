659dfe8b9fb755a0f0fd0f20fe5e72fbe452cfff
mofesilva <mofesilva.dev@gmail.com>
2025-10-10 21:11:03 +0200

    Refactor Carousel component to improve type safety by using SwiperClass type for onSlideChange and onSwiper props

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -2,6 +2,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import type { SwiperOptions } from 'swiper/types'
+import type { Swiper as SwiperClass } from 'swiper/types'
 import 'swiper/css/bundle' // CSS completo do Swiper
 
 type CarouselProps = {
@@ -9,8 +10,8 @@ type CarouselProps = {
     options?: SwiperOptions
     className?: string
     style?: React.CSSProperties
-    onSlideChange?: (swiper: any) => void
-    onSwiper?: (swiper: any) => void
+    onSlideChange?: (swiper: SwiperClass) => void
+    onSwiper?: (swiper: SwiperClass) => void
 }
 
 export default function Carousel({ slides, options, className, style, onSlideChange, onSwiper }: CarouselProps) {
@@ -21,7 +22,7 @@ export default function Carousel({ slides, options, className, style, onSlideCha
         <Swiper
             modules={[Navigation, Pagination, Autoplay]}
             navigation={enableNavigation}
-            pagination={enablePagination ? { ...(options!.pagination as any) } : false}
+            pagination={enablePagination ? { ...(options!.pagination as object) } : false}
             autoplay={options?.autoplay ?? { delay: 8000 }}
             style={style}
             loop={options?.loop ?? true}
