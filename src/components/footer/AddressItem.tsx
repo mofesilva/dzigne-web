0ca1d53f1e29f45db89922e96d1d848e7af9d5ec
mofesilva <mofesilva.dev@gmail.com>
2026-03-02 17:02:32 -0300

    style(AddressItem): downgrade h1 to h6, remove hardcoded font size

--- src/components/footer/AddressItem.tsx
+++ src/components/footer/AddressItem.tsx
@@ -12,7 +12,7 @@ interface AddressItemProps {
 function AddressItem({ title, address, city, details, state, country }: AddressItemProps) {
     return (
         <div className="text-center md:text-left">
-            <h1 className="font-rajdhani text-xl text-center font-bold text-carbon">{title}</h1>
+            <h6 className="font-rajdhani text-center font-bold text-carbon">{title}</h6>
             <p className="font-league-spartan text-center text-md/6 text-carbon text-regular ">{address}<br />
                 {details}<br />
                 {city}/{state}<br />
