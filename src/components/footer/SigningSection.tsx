db9cf0e44177eff1e9fca2ffd46302d6128043b2
mofesilva <mofesilva.dev@gmail.com>
2026-03-02 17:02:48 -0300

    refactor(SigningSection): replace FA react icon with BrandIcon component

--- src/components/footer/SigningSection.tsx
+++ src/components/footer/SigningSection.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React from 'react';
+import BrandIcon from '@/components/BrandIcon';
 
 function SigningSection() {
     return (
         <div className="signing self-center pt-10 pb-2 md:pt-0 md:pb-2">
-            <p className='font-rajdhani text-center text-carbon font-semibold'>Made with <i className="fa-brands fa-beat fa-react"></i> by Dzign-e Sistemas e Tecnolgia LTDA
+            <p className='font-rajdhani text-center text-carbon font-semibold'>Made with <BrandIcon name="react" size={16} className="inline-block animate-beat" alt="React" /> by Dzign-e Sistemas e Tecnolgia LTDA
                 16.950.862/0001-15 - Todas as imagens pertencem a Freepik/Pexels/Unsplash
             </p>
         </div>
