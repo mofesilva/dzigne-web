1cf493e0fd5c2b200b7b2d48cfe96e3990b1226c
mofesilva <mofesilva.dev@gmail.com>
2026-03-02 17:01:52 -0300

    refactor(NavigationButton): change icon prop from string to ReactNode

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -5,15 +5,15 @@ import React from 'react'
 interface NavigationButtonProps {
     href: string
     text: string
-    icon?: string
+    icon?: React.ReactNode
     inverted?: boolean
 }
 
 function NavigationButton({ href, text, icon, inverted = false }: NavigationButtonProps) {
     return (
         <Link href={href} className={`group ${inverted ? 'bg-carbon hover:border-carbon' : 'bg-green-accent hover:border-green-accent'} ${icon ? 'pr-4 pl-6' : 'px-6'} py-2 rounded-lg border-2 border-transparent hover:bg-transparent transition-colors duration-300 inline-block cursor-pointer`}>
-            <span className={`${inverted ? 'text-eggshell group-hover:text-carbon' : 'text-carbon group-hover:text-green-accent'} font-rajdhani font-semibold`}>
-                {text} {icon && <i className={`fa-solid ${icon}`}></i>}
+            <span className={`${inverted ? 'text-eggshell group-hover:text-carbon' : 'text-carbon group-hover:text-green-accent'} font-rajdhani font-semibold inline-flex items-center gap-1`}>
+                {text} {icon}
             </span>
         </Link>
     )
