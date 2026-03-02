03a7813865bddb23d7dd3c0d024d8b7b2ccd2398
mofesilva <mofesilva.dev@gmail.com>
2026-03-02 17:03:58 -0300

    style(AboutHeroSection): remove hardcoded font sizes, change p to h6

--- src/app/about-us/components/AboutHeroSection.tsx
+++ src/app/about-us/components/AboutHeroSection.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 function AboutHeroSection() {
     return (
         <div className="max-w-4xl mx-auto text-center align-center justify-center self-center pb-16">
-            <h1 className='inline-flex flex-wrap justify-center items-center text-4xl 2xl:text-5xl font-rajdhani font-bold text-white mb-6 gap-4'>
+            <h1 className='inline-flex flex-wrap justify-center items-center font-rajdhani font-bold text-white mb-6 gap-4'>
                 <span>Conheça a</span>
                 <span className="text-green-accent">Dzign-e</span>
             </h1>
 
-            <p className='text-lg 2xl:text-xl font-league-spartan text-white/80 max-w-2xl mx-auto'>
+            <h6 className='font-league-spartan text-white/80 max-w-2xl mx-auto'>
                 Uma software house brasileira especializada em criar produtos e soluções digitais sob medida para empresas que buscam inovação e resultados reais.
-            </p>
+            </h6>
         </div>
     );
 }
