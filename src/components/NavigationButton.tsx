9d21c125aaf4a3ce4f7ad1b302fbf35406418d43
mofesilva <mofesilva.dev@gmail.com>
2026-03-03 22:09:25 -0300

    refactor: update NavigationButton component

--- src/components/NavigationButton.tsx
+++ src/components/NavigationButton.tsx
@@ -12,9 +12,9 @@ interface NavigationButtonProps {
 function NavigationButton({ href, text, icon, inverted = false }: NavigationButtonProps) {
     return (
         <Link href={href} className={`group ${inverted ? 'bg-carbon hover:border-carbon' : 'bg-green-accent hover:border-green-accent'} ${icon ? 'pr-4 pl-6' : 'px-6'} py-2 rounded-lg border-2 border-transparent hover:bg-transparent transition-colors duration-300 inline-block cursor-pointer`}>
-            <span className={`${inverted ? 'text-eggshell group-hover:text-carbon' : 'text-carbon group-hover:text-green-accent'} font-rajdhani font-semibold inline-flex items-center gap-1`}>
+            <h6 className={`${inverted ? 'text-eggshell group-hover:text-carbon' : 'text-carbon group-hover:text-green-accent'} font-rajdhani font-semibold inline-flex items-center gap-1`}>
                 {text} {icon}
-            </span>
+            </h6>
         </Link>
     )
 }
