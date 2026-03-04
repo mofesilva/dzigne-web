2d7c37738e692201e228844260936dbedc2aff3b
mofesilva <mofesilva.dev@gmail.com>
2026-03-04 20:49:18 -0300

    refactor: remove HeaderSection wrapper from CasesHeroSection for cleaner structure

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import CasesHeroSection from "./components/CasesHeroSection";
 import CasesListSection from "./components/CasesListSection";
-import HeaderSection from "../components/HeaderSection";
 
 const Page = () => {
   return (
     <div className="flex flex-col">
-      <HeaderSection content={<CasesHeroSection />} />
+      <CasesHeroSection />
       <CasesListSection />
     </div>
   );
