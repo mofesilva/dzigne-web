e3635bbe1b59a15b5c8a2fb361f0dc29056dbd97
mofesilva <mofesilva.dev@gmail.com>
2026-03-03 22:07:43 -0300

    refactor: update about-us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 import AboutHeroSection from "./components/AboutHeroSection";
-import AboutMissionSection from "./components/AboutMissionSection";
-import AboutValuesSection from "./components/AboutValuesSection";
+import AboutManifestoSection from "./components/AboutManifestoSection";
+import AboutStatsSection from "./components/AboutStatsSection";
+import AboutMissionValuesSection from "./components/AboutMissionValuesSection";
+import AboutTechStackSection from "./components/AboutTechStackSection";
 import AboutTeamSection from "./components/AboutTeamSection";
-import HeaderSection from "../components/HeaderSection";
+import AboutCTASection from "./components/AboutCTASection";
+
+export const metadata = {
+  title: "Sobre Nós | Dzign-e",
+  description:
+    "Conheça a Dzign-e: nossa missão, valores, equipe e as tecnologias que utilizamos para criar soluções digitais extraordinárias.",
+};
 
 const Page = () => {
   return (
     <main className="flex flex-col">
-      <HeaderSection content={<AboutHeroSection />} />
-      <AboutMissionSection />
-      <AboutValuesSection />
+      <AboutHeroSection />
+      <AboutManifestoSection />
+      <AboutStatsSection />
+      <AboutMissionValuesSection />
+      <AboutTechStackSection />
       <AboutTeamSection />
+      <AboutCTASection />
     </main>
   );
 };
