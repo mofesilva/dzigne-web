b7379ceb93af2e8cce82aebb59a0eeadff206eae
mofesilva <mofesilva.dev@gmail.com>
2026-03-06 13:48:45 -0300

    refactor: update cases page with CasesIntroSection

--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CasesHeroSection from "./components/CasesHeroSection";
+import CasesIntroSection from "./components/CasesIntroSection";
 import CasesListSection from "./components/CasesListSection";
 
 const Page = () => {
   return (
     <div className="flex flex-col">
       <CasesHeroSection />
+      <CasesIntroSection />
       <CasesListSection />
     </div>
   );
